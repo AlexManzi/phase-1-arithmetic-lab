1ee57d03974fd6f9c1bdc0ddd5ad7700d8f58efe
Alex Manzi <alex.manzi22@gmail.com>
2022-02-07 16:11:56 -0500

    commit tree 62c48898d8565132cdf3c561dc7a07220bd15dc2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let num1 = 2
-let num2 = 31
-const multiply = (num1 * num2)
-const random = Math.ceil(Math.random())
-let num3 = 121
-let num4 = 117
-const mod = (num3 % num4)
-const max = Math.max(1, 2, 3, 5, 20)
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.ceil(Math.random())
+let num3 = 117 
+let num4 = 121
+let mod = num4 % num3
+let max = Math.max(1,2,3,20)
